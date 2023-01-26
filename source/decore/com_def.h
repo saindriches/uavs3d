ca55185b3f1977dfd4d9dee33b24e2fcdc9ec02a
Rainbaby <rainbaby@outlook.jp>
2023-01-26 19:13:15 +0800

    Fix build on macOS with Apple Silicon It also fixes 10-bit build definition mistakes on other os.
    Signed-off-by: Rainbaby <rainbaby@outlook.jp>

--- source/decore/com_def.h
+++ source/decore/com_def.h
@@ -37,25 +37,12 @@
 
 #define CHECK_RAND_STRM 0
 
-#if defined(__linux__)
 #if COMPILE_10BIT
 typedef unsigned short pel; /* pixel type */
 #define BIT_DEPTH 10
 #else
 typedef unsigned char pel; /* pixel type */
 #define BIT_DEPTH 8
-#endif
-
-#else
-#define COMPILE_10BIT 0
-#if COMPILE_10BIT
-typedef unsigned short pel;
-#define BIT_DEPTH 10
-#else
-typedef unsigned char pel;
-#define BIT_DEPTH 8
-#endif
-
 #endif
 
 /*************************  profile & level **********************************************/
